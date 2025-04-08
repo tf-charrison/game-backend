4e6f71e999a9822747817122ed9c224664c292f5
Christopher Harrison <charrison@tracefirst.com>
2025-04-08 09:25:44 +0100

    Initial commit - Rails backend API

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,35 @@
+class AuthController < ApplicationController
+  # POST /register
+  def register
+    user = User.new(user_params)
+
+    if user.save
+      render json: { message: 'User created successfully' }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # POST /login
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      token = encode_token(user.id)
+      render json: { token: token }
+    else
+      render json: { message: 'Invalid credentials' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  # Encode JWT token
+  def encode_token(user_id)
+    JWT.encode({ user_id: user_id }, Rails.application.secret_key_base, 'HS256')
+  end
+end
